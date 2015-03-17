e26586b18f77815f4d8b18c80dc9ba82c7b8d1e2
casfahrenfort <casfahrenfort@live.nl>
2015-03-17 21:13:24 +0100

    Collision met walls

--- DarkRoom/DarkRoom/DarkRoom/GameObjects/Tiles/Tile.cs
+++ DarkRoom/DarkRoom/DarkRoom/GameObjects/Tiles/Tile.cs
@@ -15,6 +15,7 @@ enum TileType
 class Tile : SpriteGameObject
 {
     public TileType type;
+    new public Rectangle BoundingBox;
 
     public Tile(Vector2 pos, TileType type)
         : base("Sprites/spr_tile", 1, "tile", 0)
@@ -22,8 +23,12 @@ class Tile : SpriteGameObject
         this.position = pos;
         this.type = type;
 
-        if (type == TileType.Door)
+        if (type == TileType.Wall)
+            visible = false;
+        else if (type == TileType.Door)
             sprite = new SpriteSheet("Sprites/spr_door");
+
+        BoundingBox = new Rectangle((int)position.X, (int)position.Y, 64, 64);
 
     }
 }
