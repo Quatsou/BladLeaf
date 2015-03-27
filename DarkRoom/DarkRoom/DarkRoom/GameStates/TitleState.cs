a2e7204045e4261cc219baae6d73e3d64c8f6679
casfahrenfort <casfahrenfort@live.nl>
2015-03-27 23:44:45 +0100

    FINAL VERSION 2

--- DarkRoom/DarkRoom/DarkRoom/GameStates/TitleState.cs
+++ DarkRoom/DarkRoom/DarkRoom/GameStates/TitleState.cs
@@ -7,7 +7,6 @@ using System.Text;
 class TitleState : GameObjectList
 {
     protected Button playbutton, quitbutton;
-    protected CheckBox fs;
     protected DarkRoom darkroom;
 
     public TitleState(DarkRoom game)
@@ -26,14 +25,6 @@ class TitleState : GameObjectList
         quitbutton.Position = new Vector2(100, 590);
         this.Add(quitbutton);
 
-        fs = new CheckBox();
-        fs.Position = new Vector2(110, 745);
-        this.Add(fs);
-
-        TextGameObject fscr = new TextGameObject("Full Screen");
-        fscr.Position = new Vector2(190, 750);
-        this.Add(fscr);
-
         GameEnvironment.AssetManager.PlayMusic("Audio/MenuLoop", true);
     }
 
@@ -46,9 +37,6 @@ class TitleState : GameObjectList
 
         if (quitbutton.Pressed)
             darkroom.Exit();
-
-        if (fs.Pressed)
-            darkroom.SetFullScreen(fs.Ticked);
 
     }
 }
